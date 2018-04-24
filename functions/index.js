f7da0868da75d28ac88829cd4b2265dbdf6c5918
Jin Park <jin@jinpark.net>
2018-04-24 18:32:22 +0900

    actually make this work

--- functions/index.js
+++ functions/index.js
@@ -2,12 +2,32 @@
 
 const {dialogflow} = require('actions-on-google');
 const functions = require('firebase-functions');
+const rp = require('request-promise-native');
+const airvisualKey = functions.config().airvisual.key;
+const mapquestKey = functions.config().mapquest.key;
 
 const app = dialogflow({debug: true});
 
+
 app.intent('get_location', (conv, {location}) => {
-  conv.close(`Alright, your location is ${location}! ` +
-    `I hope you like it. See you next time.`);
+  console.log('location', location);
+  return rp(`http://www.mapquestapi.com/geocoding/v1/address?key=${mapquestKey}&location=${location.city}`)
+  .then((body) => {
+    var resp = JSON.parse(body);
+    console.log('geocode', resp);
+    var lat = resp.results[0].locations[0].latLng.lat;
+    var lon = resp.results[0].locations[0].latLng.lng;
+    return rp(`https://api.airvisual.com/v2/nearest_city?key=${airvisualKey}&lat=${lat}&lon=${lon}`)
+      .then((body)  => {
+        var resp = JSON.parse(body);
+        console.log('airvisual', lat, lon, resp);
+        conv.close(`air quality in ${resp.data.city} is ${resp.data.current.pollution.aqius}`)
+        return;
+      });
+  })
+
+
+
 });
 
 exports.airqualityAction = functions.https.onRequest(app);
